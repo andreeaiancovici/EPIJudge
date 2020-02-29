d0f45ff8f663715baef31f678814d19ac987ded5
andreea.iancovici <andreea.iancovici@metrosystems.net>
2020-02-29 14:25:28 +0200

    Next Permutation with O(n) runtime and O(1) storage

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -1,12 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    // TODO - you fill in here.
-    return null;
+    if(perm.size() == 0) {
+      return new ArrayList<>();
+    }
+
+    int i = perm.size() - 1;
+    while(i > 0 && perm.get(i) <= perm.get(i - 1)) {
+      i--;
+    }
+
+    if(i == 0) {
+      return new ArrayList<>();
+    }
+
+    int val = perm.get(i - 1);
+    int j = perm.size() - 1;
+    while(j > i - 1 && perm.get(j) <= val) {
+      j--;
+    }
+
+    Collections.swap(perm, i - 1, j);
+    Collections.reverse(perm.subList(i, perm.size()));
+    return perm;
   }
 
   public static void main(String[] args) {

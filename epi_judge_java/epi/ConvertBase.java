9d5bcb8450943cb0dfa7d07ed9cc21ea90b98a77
andreea.iancovici <andreea.iancovici@metrosystems.net>
2020-06-13 22:36:52 +0300

    ConvertBase - runtime O(n(1 + log b2 b1)) SpreadsheetEncoding - runtime O(n)

--- epi_judge_java/epi/ConvertBase.java
+++ epi_judge_java/epi/ConvertBase.java
@@ -1,19 +1,80 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ConvertBase {
-  @EpiTest(testDataFile = "convert_base.tsv")
+    @EpiTest(testDataFile = "convert_base.tsv")
 
-  public static String convertBase(String numAsString, int b1, int b2) {
-    // TODO - you fill in here.
-    return "";
-  }
+    public static String convertBase(String numAsString, int b1, int b2) {
+        int numInBaseTen = 0;
+        boolean isNegative = false;
+        for (int i = numAsString.length() - 1, j = 0; i >= 0; i--, j++) {
+            if (i == 0 && numAsString.charAt(i) == '-') {
+                isNegative = true;
+            } else {
+                char c = numAsString.charAt(i);
+                numInBaseTen += (((c - '0') > 9 ? getValue(c) : c - '0') * Math.pow(b1, j));
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if(numInBaseTen == 0) {
+          stringBuilder.append("0");
+        }
+        while (numInBaseTen != 0) {
+            int number = numInBaseTen % b2;
+            stringBuilder.append(number > 9 ? getLetter(number) : number);
+            numInBaseTen /= b2;
+        }
+        if (isNegative) {
+            stringBuilder.append("-");
+        }
+        stringBuilder.reverse();
+        return stringBuilder.toString();
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ConvertBase.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static String getLetter(int number) {
+        switch (number) {
+            case 10:
+                return "A";
+            case 11:
+                return "B";
+            case 12:
+                return "C";
+            case 13:
+                return "D";
+            case 14:
+                return "E";
+            case 15:
+                return "F";
+        }
+        return "";
+    }
+
+    private static int getValue(char letter) {
+        switch (letter) {
+            case 'A':
+                return 10;
+            case 'B':
+                return 11;
+            case 'C':
+                return 12;
+            case 'D':
+                return 13;
+            case 'E':
+                return 14;
+            case 'F':
+                return 15;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ConvertBase.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/SpreadsheetEncoding.java
+++ epi_judge_java/epi/SpreadsheetEncoding.java
@@ -1,19 +1,83 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SpreadsheetEncoding {
-  @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
+    @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
-  public static int ssDecodeColID(final String col) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    public static int ssDecodeColID(final String col) {
+        int id = 0;
+        for (int i = col.length() - 1, j = 0; i >= 0; i--, j++) {
+          id += (getNumber(col.charAt(i)) * Math.pow(26, j));
+        }
+        return id;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SpreadsheetEncoding.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    private static int getNumber(char letter) {
+        switch (letter) {
+            case 'A':
+                return 1;
+            case 'B':
+                return 2;
+            case 'C':
+                return 3;
+            case 'D':
+                return 4;
+            case 'E':
+                return 5;
+            case 'F':
+                return 6;
+            case 'G':
+                return 7;
+            case 'H':
+                return 8;
+            case 'I':
+                return 9;
+            case 'J':
+                return 10;
+            case 'K':
+                return 11;
+            case 'L':
+                return 12;
+            case 'M':
+                return 13;
+            case 'N':
+                return 14;
+            case 'O':
+                return 15;
+            case 'P':
+                return 16;
+            case 'Q':
+                return 17;
+            case 'R':
+                return 18;
+            case 'S':
+                return 19;
+            case 'T':
+                return 20;
+            case 'U':
+                return 21;
+            case 'V':
+                return 22;
+            case 'W':
+                return 23;
+            case 'X':
+                return 24;
+            case 'Y':
+                return 25;
+            case 'Z':
+                return 26;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "SpreadsheetEncoding.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }

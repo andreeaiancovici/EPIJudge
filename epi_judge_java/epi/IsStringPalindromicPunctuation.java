d517d32419ef526154d1cbc687717a7c2f42a45f
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-06-28 15:26:36 +0300

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/IsStringPalindromicPunctuation.java
+++ epi_judge_java/epi/IsStringPalindromicPunctuation.java
@@ -5,8 +5,28 @@ public class IsStringPalindromicPunctuation {
   @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
 
   public static boolean isPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
+    int start = 0, end = s.length() - 1;
+    boolean isPalindrome = true;
+    while(start <= end) {
+      if(Character.isLetterOrDigit(s.charAt(start)) && Character.isLetterOrDigit(s.charAt(end))) {
+        if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+          isPalindrome = false;
+          break;
+        } else {
+          start++;
+          end--;
+        }
+      }
+      else if(Character.isLetterOrDigit(s.charAt(start))) {
+        end--;
+      } else if(Character.isLetterOrDigit(s.charAt(end))) {
+        start++;
+      } else {
+        start++;
+        end--;
+      }
+    }
+    return isPalindrome;
   }
 
   public static void main(String[] args) {

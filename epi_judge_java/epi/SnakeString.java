39c27df6957d50d7459958e9b213d0c57ad31a51
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-07-12 16:07:29 +0300

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/SnakeString.java
+++ epi_judge_java/epi/SnakeString.java
@@ -5,8 +5,17 @@ public class SnakeString {
   @EpiTest(testDataFile = "snake_string.tsv")
 
   public static String snakeString(String s) {
-    // TODO - you fill in here.
-    return "";
+    StringBuilder sb = new StringBuilder();
+    snakeString(s, 1, 4, sb);
+    snakeString(s, 0, 2, sb);
+    snakeString(s, 3, 4, sb);
+    return sb.toString();
+  }
+
+  private static void snakeString(String s, int index, int step, StringBuilder sb) {
+    for (int i = index; i < s.length(); i += step) {
+      sb.append(s.charAt(i));
+    }
   }
 
   public static void main(String[] args) {

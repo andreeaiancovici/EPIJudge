26907f3fa817dc5c4808360793f479fb8eb32aea
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-07-26 22:08:51 +0300

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/IsListPalindromic.java
+++ epi_judge_java/epi/IsListPalindromic.java
@@ -5,8 +5,39 @@ public class IsListPalindromic {
   @EpiTest(testDataFile = "is_list_palindromic.tsv")
 
   public static boolean isLinkedListAPalindrome(ListNode<Integer> L) {
-    // TODO - you fill in here.
+    ListNode<Integer> oneStep = L;
+    ListNode<Integer> twoStep = L;
+    while(oneStep != null && twoStep != null && twoStep.next != null) {
+      oneStep = oneStep.next;
+      twoStep = twoStep.next.next;
+    }
+    ListNode<Integer> firstHalf = L;
+    ListNode<Integer> secondHalf = reverse(oneStep);
+    while(firstHalf != null && secondHalf != null) {
+      if(!firstHalf.data.equals(secondHalf.data)) {
+        return false;
+      }
+      firstHalf = firstHalf.next;
+      secondHalf = secondHalf.next;
+    }
     return true;
+  }
+
+  private static ListNode<Integer> reverse(ListNode<Integer> list) {
+    ListNode<Integer> prev = null;
+    while(list != null) {
+      if(prev != null) {
+        ListNode<Integer> temp = list.next;
+        list.next = prev;
+        prev = list;
+        list = temp;
+      } else {
+        prev = list;
+        list = list.next;
+        prev.next = null;
+      }
+    }
+    return prev;
   }
 
   public static void main(String[] args) {

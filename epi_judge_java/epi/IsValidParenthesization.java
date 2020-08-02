691aaddfd3a4236fbbc59d73fddeaac965d23ae6
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-03 00:19:45 +0300

    O(n) time complexity

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,12 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.*;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
+    Deque<Character> stack = new LinkedList<>();
+    for (int i = 0; i < s.length(); i++) {
+      char bracket = s.charAt(i);
+      if ("{[(".indexOf(bracket) != -1) {
+        stack.push(bracket);
+      } else {
+        if (!stack.isEmpty()) {
+          if (bracket == '}' && stack.peek() != '{') {
+            return false;
+          } else if (bracket == ']' && stack.peek() != '[') {
+            return false;
+          } else if (bracket == ')' && stack.peek() != '(') {
+            return false;
+          } else {
+            stack.pop();
+          }
+        } else return false;
+      }
+    }
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {

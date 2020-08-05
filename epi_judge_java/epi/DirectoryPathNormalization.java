13b2ee96d9996f5b82bdbd13d808d4815e02028f
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-05 23:22:52 +0300

    O(n) time complexity

--- epi_judge_java/epi/DirectoryPathNormalization.java
+++ epi_judge_java/epi/DirectoryPathNormalization.java
@@ -5,8 +5,33 @@ public class DirectoryPathNormalization {
   @EpiTest(testDataFile = "directory_path_normalization.tsv")
 
   public static String shortestEquivalentPath(String path) {
-    // TODO - you fill in here.
-    return "";
+    StringBuilder normalizedPath = new StringBuilder();
+    if (path.startsWith("/")) {
+      normalizedPath.append("/");
+    }
+    Deque<String> folders = new LinkedList<>();
+    String[] items = path.split("/");
+    for (String item : items) {
+      if (item.equals("..")) {
+        String lastItem = folders.peek();
+        if (lastItem != null && !lastItem.equals("..")) {
+          folders.pop();
+        } else {
+          folders.push(item);
+        }
+      } else if (!item.equals(".") && !item.isEmpty()) {
+        folders.push(item);
+      }
+    }
+    if (!folders.isEmpty()) {
+      Iterator<String> it = folders.descendingIterator();
+      normalizedPath.append(it.next());
+      while (it.hasNext()) {
+        normalizedPath.append("/");
+        normalizedPath.append(it.next());
+      }
+    }
+    return normalizedPath.toString();
   }
 
   public static void main(String[] args) {

1f82804a1b3b95edf3687111469b31dae13032c2
andreea.iancovici <andreea.iancovici@metrosystems.net>
2020-08-09 22:58:45 +0300

    O(n) time complexity and O(m) space complexity, where m is the maximum size of a level

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -1,14 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 public class TreeLevelOrder {
-  @EpiTest(testDataFile = "tree_level_order.tsv")
 
-  public static List<List<Integer>>
-  binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+  @EpiTest(testDataFile = "tree_level_order.tsv")
+  public static List<List<Integer>> binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
+    List<List<Integer>> results = new ArrayList<>();
+    Deque<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+    queue.addLast(tree);
+    while(!queue.isEmpty()) {
+      List<Integer> result = new ArrayList<>();
+      Deque<BinaryTreeNode<Integer>> nextQueue = new LinkedList<>();
+      while(!queue.isEmpty()) {
+         BinaryTreeNode<Integer> node = queue.pollFirst();
+         if(node != null) {
+           result.add(node.data);
+           nextQueue.addLast(node.left);
+           nextQueue.addLast(node.right);
+         }
+      }
+      if(!result.isEmpty()) {
+        results.add(result);
+      }
+      queue = nextQueue;
+    }
+    return results;
   }
 
   public static void main(String[] args) {

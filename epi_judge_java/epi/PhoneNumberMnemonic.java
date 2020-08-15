29245968732f49a141fc17ace6d0be2a6b7b05d8
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-15 14:38:14 +0300

    O(4^n) time complexity and space complexity

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -2,16 +2,142 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 import java.util.function.BiPredicate;
 public class PhoneNumberMnemonic {
   @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
 
   public static List<String> phoneMnemonic(String phoneNumber) {
-    // TODO - you fill in here.
-    return null;
+    Set<String> results = new HashSet<>();
+    recursivePhoneMnemonic(results, phoneNumber, 0, new StringBuilder());
+    return new ArrayList<>(results);
   }
+
+  private static void recursivePhoneMnemonic(Set<String> results, String phoneNumber, int index, StringBuilder sb) {
+    if (index == phoneNumber.length()) {
+      results.add(sb.toString());
+    } else {
+      for (int j = 0; j < 4; j++) {
+        String letter = getLetter(phoneNumber.substring(index, index + 1), j);
+        if (letter != null) {
+          sb.append(letter);
+          recursivePhoneMnemonic(results, phoneNumber, index + 1, sb);
+          sb.delete(sb.lastIndexOf(letter), sb.length());
+        }
+      }
+    }
+  }
+
+  private static String getLetter(String number, int index) {
+    switch (number) {
+      case "0": return "0";
+      case "1": return "1";
+      case "2": {
+        switch (index) {
+          case 0:
+            return "A";
+          case 1:
+            return "B";
+          case 2:
+            return "C";
+          default:
+            return null;
+        }
+      }
+      case "3": {
+        switch (index) {
+          case 0:
+            return "D";
+          case 1:
+            return "E";
+          case 2:
+            return "F";
+          default:
+            return null;
+        }
+      }
+      case "4": {
+        switch (index) {
+          case 0:
+            return "G";
+          case 1:
+            return "H";
+          case 2:
+            return "I";
+          default:
+            return null;
+        }
+      }
+      case "5": {
+        switch (index) {
+          case 0:
+            return "J";
+          case 1:
+            return "K";
+          case 2:
+            return "L";
+          default:
+            return null;
+        }
+      }
+      case "6": {
+        switch (index) {
+          case 0:
+            return "M";
+          case 1:
+            return "N";
+          case 2:
+            return "O";
+          default:
+            return null;
+        }
+      }
+      case "7": {
+        switch (index) {
+          case 0:
+            return "P";
+          case 1:
+            return "Q";
+          case 2:
+            return "R";
+          case 3:
+            return "S";
+          default:
+            return null;
+        }
+      }
+      case "8": {
+        switch (index) {
+          case 0:
+            return "T";
+          case 1:
+            return "U";
+          case 2:
+            return "V";
+          default:
+            return null;
+        }
+      }
+      case "9": {
+        switch (index) {
+          case 0:
+            return "W";
+          case 1:
+            return "X";
+          case 2:
+            return "Y";
+          case 3:
+            return "Z";
+          default:
+            return null;
+        }
+      }
+      default:
+        return null;
+    }
+  }
+
   @EpiTestComparator
   public static BiPredicate<List<String>, List<String>> comp =
       (expected, result) -> {

9861fb075705a3446d838a6e8cd8ddbac8b27b10
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-16 16:57:22 +0300

    O(h) space complexity and O(n) time complexity

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -5,8 +5,20 @@ public class IsTreeSymmetric {
   @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if(tree == null) {
+      return true;
+    }
+    return checkSymmetric(tree.left, tree.right);
+  }
+
+  private static boolean checkSymmetric(BinaryTreeNode<Integer> left, BinaryTreeNode<Integer> right) {
+    if(left == null && right == null) {
+      return true;
+    }
+    if(left != null && right != null) {
+      return left.data.equals(right.data) && checkSymmetric(left.left, right.right) && checkSymmetric(left.right, right.left);
+    }
+    return false;
   }
 
   public static void main(String[] args) {

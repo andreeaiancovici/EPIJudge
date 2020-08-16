4a22b6d82a1a7dcffbb089ef6ff07acf84f16bf5
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-16 16:43:50 +0300

    O(h) space complexity and O(n) time complexity

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,13 +1,34 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.concurrent.atomic.*;
+
 public class IsTreeBalanced {
 
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if(tree == null) {
+      return true;
+    }
+    if(!isBalanced(tree.left)) {
+      return false;
+    }
+    int leftHeight = computeHeight(tree.left);
+    if(!isBalanced(tree.right)) {
+      return false;
+    }
+    int rightHeight = computeHeight(tree.right);
+
+    return Math.abs(leftHeight - rightHeight) <= 1;
+  }
+
+  private static int computeHeight(BinaryTreeNode<Integer> tree) {
+    if (tree == null) {
+      return 0;
+    }
+    return Math.max(computeHeight(tree.left), computeHeight(tree.right)) + 1;
   }
 
   public static void main(String[] args) {

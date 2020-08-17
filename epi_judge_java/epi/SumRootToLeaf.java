95c2bc5d4b4f0a2940e499db0186f2590c78bd53
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-17 10:12:06 +0300

    O(n) time complexity and O(h) space complexity, where h is the height of the tree

--- epi_judge_java/epi/SumRootToLeaf.java
+++ epi_judge_java/epi/SumRootToLeaf.java
@@ -1,12 +1,27 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.*;
+
 public class SumRootToLeaf {
   @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+     return inOrder(0, tree);
+  }
+
+  private static int inOrder(int sum, BinaryTreeNode<Integer> tree) {
+    if(tree == null) {
+      return 0;
+    } else {
+      sum *= 2;
+      sum += tree.data;
+      if(tree.left == null && tree.right == null) {
+        return sum;
+      }
+      return inOrder(sum, tree.left) + inOrder(sum, tree.right);
+    }
   }
 
   public static void main(String[] args) {

f8d4c02f58c57d5fd7bd43a930d745fd734d672f
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-17 09:22:59 +0300

    O(h) time complexity and O(1) space complexity, where h is the height of the tree

--- epi_judge_java/epi/LowestCommonAncestorWithParent.java
+++ epi_judge_java/epi/LowestCommonAncestorWithParent.java
@@ -8,9 +8,34 @@ public class LowestCommonAncestorWithParent {
 
   public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
+    int depth0 = getDepth(node0);
+    int depth1 = getDepth(node1);
+    if (depth0 < depth1) {
+      while (depth0 < depth1) {
+        node1 = node1.parent;
+        depth1--;
+      }
+    } else if (depth0 > depth1) {
+      while (depth0 > depth1) {
+        node0 = node0.parent;
+        depth0--;
+      }
+    }
+    while(node0 != node1) {
+      node0 = node0.parent;
+      node1 = node1.parent;
+    }
+    return node0;
   }
+
+  private static int getDepth(BinaryTree<Integer> node) {
+    if(node.parent == null) {
+      return 0;
+    } else {
+      return getDepth(node.parent) + 1;
+    }
+  }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
                                Integer key0, Integer key1) throws Exception {

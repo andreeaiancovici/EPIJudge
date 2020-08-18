c65d3a6ecd0c10dadb2e5322aedebd34abedd9b3
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-18 09:45:54 +0300

    O(n) time complexity and O(h) space complexity, where h is the height of the tree

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,13 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class TreeInorder {
   @EpiTest(testDataFile = "tree_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> inorder = new ArrayList<>();
+    if(tree != null) {
+      Deque<BinaryTreeNode<Integer>> stack = new LinkedList<>();
+      BinaryTreeNode<Integer> node = tree;
+      while(!stack.isEmpty() || node != null) {
+        if(node != null) {
+          stack.push(node);
+          node = node.left;
+        } else {
+          node = stack.pop();
+          inorder.add(node.data);
+          node = node.right;
+        }
+      }
+    }
+    return inorder;
   }
 
   public static void main(String[] args) {

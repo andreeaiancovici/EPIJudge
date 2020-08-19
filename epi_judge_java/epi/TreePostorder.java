4c6bdef85600a3411f37a013a95b483f0efc9725
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-19 09:30:44 +0300

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/TreePostorder.java
+++ epi_judge_java/epi/TreePostorder.java
@@ -1,14 +1,34 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class TreePostorder {
   @EpiTest(testDataFile = "tree_postorder.tsv")
 
   // We use stack and previous node pointer to simulate postorder traversal.
   public static List<Integer> postorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> postOrder = new ArrayList<>();
+    if (tree != null) {
+      Deque<BinaryTreeNode<Integer>> stack = new LinkedList<>();
+      BinaryTreeNode<Integer> node = tree;
+      while(!stack.isEmpty() || node != null) {
+        if(node != null) {
+          stack.push(node);
+          node = node.left;
+        } else {
+          node = stack.pop();
+          if(node.right == null) {
+            postOrder.add(node.data);
+          } else {
+            stack.push(node);
+          }
+          node = node.right;
+        }
+      }
+    }
+    return postOrder;
   }
 
   public static void main(String[] args) {

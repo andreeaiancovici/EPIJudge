618819266c2f8b0f59742b661220795a371c0753
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-19 09:30:57 +0300

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -1,13 +1,41 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class TreeWithParentInorder {
   @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>();
+    boolean leftDone = false;
+    while (tree != null) {
+      if (!leftDone) {
+        while (tree.left != null) {
+          tree = tree.left;
+        }
+      }
+      result.add(tree.data);
+      leftDone = true;
+      if (tree.right != null) {
+        tree = tree.right;
+        leftDone = false;
+      } else if (tree.parent != null) {
+        if (tree == tree.parent.left) {
+          tree = tree.parent;
+        } else {
+          while (tree.parent != null && tree == tree.parent.right) {
+            tree = tree.parent;
+          }
+          if (tree.parent == null) {
+            break;
+          }
+          tree = tree.parent;
+        }
+      } else break;
+    }
+    return result;
   }
 
   public static void main(String[] args) {

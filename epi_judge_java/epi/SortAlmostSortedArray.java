bc2af50ce7d404da39b28102a7caca150b9dd16e
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-29 17:05:38 +0300

    O(k) space complexity and O(nlogk) time complexity

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -1,14 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.Iterator;
-import java.util.List;
+
+import java.util.*;
+
 public class SortAlmostSortedArray {
 
   public static List<Integer>
   sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>();
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(k + 1);
+    for(int i = 0; i <= k && sequence.hasNext(); i++) {
+      minHeap.add(sequence.next());
+    }
+    while(!minHeap.isEmpty()) {
+      Integer item = minHeap.poll();
+      result.add(item);
+      if(sequence.hasNext()) {
+        minHeap.add(sequence.next());
+      }
+    }
+    return result;
   }
   @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
   public static List<Integer>

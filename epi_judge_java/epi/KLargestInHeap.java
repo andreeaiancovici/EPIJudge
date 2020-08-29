d5a74e045b92355cb505d6dd75967891befee557
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-29 22:19:42 +0300

    O(k) space complexity and O(klogk) time complexity

--- epi_judge_java/epi/KLargestInHeap.java
+++ epi_judge_java/epi/KLargestInHeap.java
@@ -2,16 +2,40 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 import java.util.function.BiPredicate;
 public class KLargestInHeap {
+
+  static class HeapEntry {
+    int value;
+    int index;
+
+    HeapEntry(int value, int index) {
+      this.value = value;
+      this.index = index;
+    }
+  }
+
   @EpiTest(testDataFile = "k_largest_in_heap.tsv")
 
   public static List<Integer> kLargestInBinaryHeap(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>(k);
+    PriorityQueue<HeapEntry> maxHeap = new PriorityQueue<>(3, (o1, o2) -> Integer.compare(o2.value, o1.value));
+    maxHeap.add(new HeapEntry(A.get(0), 0));
+    while(!maxHeap.isEmpty() && result.size() < k) {
+        HeapEntry maxCurrent = maxHeap.poll();
+        if(maxCurrent != null) {
+          result.add(maxCurrent.value);
+          if(A.size() > 2 * maxCurrent.index + 1) {
+            maxHeap.add(new HeapEntry(A.get(2 * maxCurrent.index + 1), 2 * maxCurrent.index + 1));
+          }
+          if(A.size() > 2 * maxCurrent.index + 2) {
+            maxHeap.add(new HeapEntry(A.get(2 * maxCurrent.index + 2), 2 * maxCurrent.index + 2));
+          }
+        }
+    }
+    return result;
   }
   @EpiTestComparator
   public static BiPredicate<List<Integer>, List<Integer>> comp =

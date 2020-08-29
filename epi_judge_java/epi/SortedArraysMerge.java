f7e5212c47d81f49ec038c9c25de9251c008682b
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-08-29 16:41:29 +0300

    O(k) space complexity and O(nlogk) time complexity

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -1,14 +1,48 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+import java.util.concurrent.atomic.*;
+
 public class SortedArraysMerge {
+
+  static class ArrayEntry {
+
+    int value;
+    int id;
+
+    ArrayEntry(int value, int id) {
+      this.value = value;
+      this.id = id;
+    }
+  }
+
   @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>();
+    PriorityQueue<ArrayEntry> minHeap = new PriorityQueue<>(sortedArrays.size(), Comparator.comparingInt(o -> o.value));
+
+    List<Iterator> iterators = new ArrayList<>(sortedArrays.size());
+    for(int i = 0; i < sortedArrays.size(); i++) {
+      Iterator<Integer> iterator = sortedArrays.get(i).iterator();
+      if(iterator.hasNext()) {
+        minHeap.add(new ArrayEntry(iterator.next(), i));
+      }
+      iterators.add(iterator);
+    }
+
+    while(!minHeap.isEmpty()) {
+      ArrayEntry arrayEntry = minHeap.poll();
+      result.add(arrayEntry.value);
+      if(iterators.get(arrayEntry.id).hasNext()) {
+        minHeap.add(new ArrayEntry((Integer) iterators.get(arrayEntry.id).next(), arrayEntry.id));
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {

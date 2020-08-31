83790b6b5f9b7518f26ee9e08191fa8a63f184ec
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-01 00:28:08 +0300

    O(1) space complexity and O(logn) time complexity

--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -6,8 +6,18 @@ public class SearchShiftedSortedArray {
   @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
 
   public static int searchSmallest(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    int start = 0, end = A.size() - 1, m;
+    while(start <= end) {
+      m = ((end - start) / 2) + start;
+      if (A.get(m) > A.get(A.size() - 1)) {
+        start = m + 1;
+      } else if (A.get(m).equals(A.get(A.size() - 1))) {
+        return m;
+      } else {
+        end = m - 1;
+      }
+    }
+    return start;
   }
 
   public static void main(String[] args) {

b06445a63f70ba3634feab3f57724963a29b4c03
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-20 22:47:06 +0300

    O(1) space complexity and O(n) time complexity

--- epi_judge_java/epi/SearchForMissingElement.java
+++ epi_judge_java/epi/SearchForMissingElement.java
@@ -48,8 +48,41 @@ public class SearchForMissingElement {
   @EpiTest(testDataFile = "find_missing_and_duplicate.tsv")
 
   public static DuplicateAndMissing findDuplicateMissing(List<Integer> A) {
-    // TODO - you fill in here.
-    return new DuplicateAndMissing(0, 0);
+    DuplicateAndMissing duplicateAndMissing = new DuplicateAndMissing(0, 0);
+    int initXor = 0, currXor = 0;
+    for (int i = 0; i < A.size(); i++) {
+      initXor ^= i;
+      currXor ^= A.get(i);
+    }
+    int xor = initXor ^ currXor;
+    int count = 0;
+    while ((xor >>> count & 1) != 1) {
+      count++;
+    }
+    int initXorPart = 0, currXorPart = 0;
+    for (int i = 0; i < A.size(); i++) {
+      if ((i >>> count & 1) == 1) {
+        initXorPart ^= i;
+      }
+      if ((A.get(i) >>> count & 1) == 1) {
+        currXorPart ^= A.get(i);
+      }
+    }
+    int n = initXorPart ^ currXorPart;
+    int m = xor ^ n;
+    for (Integer integer : A) {
+      if (integer == n) {
+        duplicateAndMissing.duplicate = n;
+        duplicateAndMissing.missing = m;
+        break;
+      }
+      if (integer == m) {
+        duplicateAndMissing.duplicate = m;
+        duplicateAndMissing.missing = n;
+        break;
+      }
+    }
+    return duplicateAndMissing;
   }
 
   public static void main(String[] args) {

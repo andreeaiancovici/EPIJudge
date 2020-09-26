b8c683069168e8c872dc4ccbe0cdc567ad04d671
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-27 00:22:56 +0300

    O(c) space complexity and O(n) time complexity, where c is the number of distinct letters in s

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,11 +1,31 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.*;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
+    Map<Character, Integer> letters = new HashMap<>();
+    char[] chars = s.toCharArray();
+    for (char c : chars) {
+      if (letters.containsKey(c)) {
+        letters.put(c, letters.get(c) + 1);
+      } else {
+        letters.put(c, 1);
+      }
+    }
+    int oddCount = 0;
+    for (int value : letters.values()) {
+      if (value % 2 != 0) {
+        if (oddCount >= 1) {
+          return false;
+        }
+        oddCount++;
+      }
+    }
     return true;
   }
 

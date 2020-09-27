2e558b7096eab23ae7f262b9d78f94b47d2937a2
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-27 12:18:41 +0300

    O(L) space complexity and O(n + m) time complexity, where L is the number of distinct letters in n, n is the letter size and m is the magazine size

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,13 +1,42 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.*;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
   public static boolean isLetterConstructibleFromMagazine(String letterText,
                                                           String magazineText) {
-    // TODO - you fill in here.
-    return true;
+    Map<Character, Integer> charFrequencies = new HashMap<>();
+    for (int i = 0; i < letterText.length(); i++) {
+      char c = letterText.charAt(i);
+      if (charFrequencies.containsKey(c)) {
+        charFrequencies.put(c, charFrequencies.get(c) + 1);
+      } else {
+        charFrequencies.put(c, 1);
+      }
+    }
+    for (int i = 0; i < magazineText.length(); i++) {
+      char c = magazineText.charAt(i);
+      if (c != ' ') {
+        if (charFrequencies.containsKey(c)) {
+          int count = charFrequencies.get(c);
+          if (count > 0) {
+            if (count - 1 == 0) {
+              charFrequencies.remove(c);
+            } else {
+              charFrequencies.put(c, count - 1);
+            }
+          }
+        }
+      }
+      if(charFrequencies.isEmpty()) {
+        return true;
+      }
+    }
+    return charFrequencies.isEmpty();
   }
 
   public static void main(String[] args) {

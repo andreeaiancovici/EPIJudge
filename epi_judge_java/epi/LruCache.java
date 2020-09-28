40154759c89a6bf23648191b9d6ba0298889a360
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-29 01:42:34 +0300

    O(m) space complexity and O(1) time complexity, where m is the number of unique insert operations

--- epi_judge_java/epi/LruCache.java
+++ epi_judge_java/epi/LruCache.java
@@ -3,22 +3,127 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
-import java.util.List;
+
+import java.util.*;
 
 public class LruCache {
-  LruCache(final int capacity) {}
+
+  static class IsbnInfo {
+    IsbnInfo prev;
+    IsbnInfo next;
+    int key;
+    int price;
+
+    IsbnInfo(IsbnInfo prev, IsbnInfo next, int key, int price) {
+      this.prev = prev;
+      this.next = next;
+      this.key = key;
+      this.price = price;
+    }
+  }
+  private Map<Integer, IsbnInfo> isbnCache;
+  private int capacity;
+  private IsbnInfo first;
+  private IsbnInfo last;
+
+  LruCache(final int capacity) {
+    this.isbnCache = new HashMap<>(capacity);
+    this.capacity = capacity;
+  }
+
   public Integer lookup(Integer key) {
-    // TODO - you fill in here.
-    return 0;
+    if (isbnCache.containsKey(key)) {
+      return getFirst(key).price;
+    } else return -1;
   }
+
+  private IsbnInfo getFirst(Integer key) {
+    IsbnInfo node = isbnCache.get(key);
+    if (node == first) {
+      return node;
+    } else if (node == last) {
+      IsbnInfo tempLastPrev = node.prev;
+
+      if (tempLastPrev != null) {
+        tempLastPrev.next = null;
+        node.prev = null;
+      }
+
+      node.next = first;
+      first.prev = node;
+      first = node;
+
+      last = tempLastPrev;
+
+      return first;
+    } else {
+      IsbnInfo tempNext = node.next;
+      IsbnInfo tempPrev = node.prev;
+
+      tempPrev.next = tempNext;
+      tempNext.prev = tempPrev;
+
+      node.next = first;
+      first.prev = node;
+      first = node;
+      return first;
+    }
+  }
+
   public void insert(Integer key, Integer value) {
-    // TODO - you fill in here.
-    return;
+    if (!isbnCache.containsKey(key)) {
+      if (isbnCache.size() == capacity) {
+        isbnCache.remove(last.key);
+        last = last.prev;
+        if (last != null) {
+          last.next = null;
+        } else {
+          first = null;
+        }
+      }
+      IsbnInfo node = new IsbnInfo(null, null, key, value);
+      if (first != null) {
+        node.next = first;
+        first.prev = node;
+      }
+      first = node;
+      if (last == null) {
+        last = node;
+      }
+      isbnCache.put(key, node);
+    } else {
+      getFirst(key);
+    }
   }
+
   public Boolean erase(Object key) {
-    // TODO - you fill in here.
-    return true;
+    if (isbnCache.containsKey(key)) {
+      IsbnInfo node = isbnCache.remove(key);
+      if (node == first) {
+        first = first.next;
+        if (first != null) {
+          first.prev = null;
+        } else {
+          last = null;
+        }
+      } else if (node == last) {
+        last = last.prev;
+        if (last != null) {
+          last.next = null;
+        } else {
+          first = null;
+        }
+      } else {
+        IsbnInfo tempNext = node.next;
+        IsbnInfo tempPrev = node.prev;
+
+        tempPrev.next = tempNext;
+        tempNext.prev = tempPrev;
+      }
+      return true;
+    } else return false;
   }
+
   @EpiUserType(ctorParams = {String.class, int.class, int.class})
   public static class Op {
     String code;

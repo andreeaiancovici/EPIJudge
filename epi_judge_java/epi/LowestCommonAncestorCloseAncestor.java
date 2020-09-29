a6199765eb09581cdf30a0a96835faa3513a5c76
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-09-29 21:33:57 +0300

    O(D0 + D1) space complexity and time complexity, where D0 and D1 is the distance from n0 and n1 to the common ancestor

--- epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
@@ -4,11 +4,32 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
+import java.util.*;
+
 public class LowestCommonAncestorCloseAncestor {
 
   public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
+    Set<BinaryTree<Integer>> nodes = new HashSet<>();
+    while(node0 != null || node1 != null) {
+      if (node0 != null) {
+        if (nodes.contains(node0)) {
+          return node0;
+        } else {
+          nodes.add(node0);
+        }
+        node0 = node0.parent;
+      }
+      if (node1 != null) {
+        if (nodes.contains(node1)) {
+          return node1;
+        } else {
+          nodes.add(node1);
+        }
+        node1 = node1.parent;
+      }
+    }
     return null;
   }
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")

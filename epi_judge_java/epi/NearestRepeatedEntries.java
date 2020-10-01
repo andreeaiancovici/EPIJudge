c3342b5b9ba0c79eb412ff012318bf90c4dffde6
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-10-01 21:08:42 +0300

    O(d) space complexity and O(n) time complexity, where d is the number of distinct values in the string array

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -1,13 +1,24 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
   public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
+    Map<String, Integer> distances = new HashMap<>();
+    int closestDistance = Integer.MAX_VALUE;
+    for (int i = 0; i < paragraph.size(); i++) {
+      String word = paragraph.get(i);
+      if (distances.containsKey(word)) {
+        int distance = distances.get(word);
+        closestDistance = Math.min(i - distance, closestDistance);
+      }
+      distances.put(word, i);
+    }
+    return closestDistance == Integer.MAX_VALUE ? -1 : closestDistance;
   }
 
   public static void main(String[] args) {

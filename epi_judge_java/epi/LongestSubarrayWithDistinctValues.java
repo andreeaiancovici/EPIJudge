8a5d7fd04845bd15d4b87692771d81d160a4b075
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-10-13 23:07:18 +0300

    O(m) space complexity and O(n) time complexity, where m is the number of distinct values and n is the length of the array

--- epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
+++ epi_judge_java/epi/LongestSubarrayWithDistinctValues.java
@@ -1,13 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class LongestSubarrayWithDistinctValues {
   @EpiTest(testDataFile = "longest_subarray_with_distinct_values.tsv")
 
   public static int longestSubarrayWithDistinctEntries(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    Map<Integer, Integer> lastOccurrences = new HashMap<>();
+    int currentSubarray = 0, longestSubarray = 0, startSubarray = 0;
+    for(int i = 0; i < A.size(); i++) {
+      if(lastOccurrences.containsKey(A.get(i)) && lastOccurrences.get(A.get(i)) >= startSubarray) {
+        int lastOccurrence = lastOccurrences.get(A.get(i));
+        currentSubarray = i - lastOccurrence;
+        startSubarray = lastOccurrence + 1;
+      } else {
+        currentSubarray++;
+      }
+      lastOccurrences.put(A.get(i), i);
+      if(currentSubarray > longestSubarray) {
+        longestSubarray = currentSubarray;
+      }
+    }
+    return longestSubarray;
   }
 
   public static void main(String[] args) {

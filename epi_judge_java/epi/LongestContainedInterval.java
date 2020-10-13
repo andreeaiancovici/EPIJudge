73e2a0c3c68cce050768730adc7f5469a6b22c9a
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-10-14 00:02:10 +0300

    O(n) space complexity and O(n) time complexity

--- epi_judge_java/epi/LongestContainedInterval.java
+++ epi_judge_java/epi/LongestContainedInterval.java
@@ -1,13 +1,38 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class LongestContainedInterval {
   @EpiTest(testDataFile = "longest_contained_interval.tsv")
 
   public static int longestContainedRange(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    Set<Integer> items = new HashSet<>(A);
+    int longestCR = 0, currentCR = 0;
+    for (Integer integer : A) {
+      if (items.contains(integer)) {
+        currentCR++;
+        items.remove(integer);
+        int upper = integer + 1;
+        while (items.contains(upper)) {
+          currentCR++;
+          items.remove(upper);
+          upper++;
+        }
+        int lower = integer - 1;
+        while (items.contains(lower)) {
+          currentCR++;
+          items.remove(lower);
+          lower--;
+        }
+        if (currentCR > longestCR) {
+          longestCR = currentCR;
+        }
+        currentCR = 0;
+      }
+    }
+    return longestCR;
   }
 
   public static void main(String[] args) {

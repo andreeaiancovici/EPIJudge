094a32613acd47c73a5914b171cf66ae93ac0e83
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-10-13 22:20:08 +0300

    O(m) space complexity and O(n) time complexity, where m is the number of values in the keywords and n is the length of the paragraph

--- epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
+++ epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
@@ -3,7 +3,9 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
-import java.util.List;
+
+import java.util.*;
+
 public class SmallestSubarrayCoveringAllValues {
 
   public static class Subarray {
@@ -20,8 +22,38 @@ public class SmallestSubarrayCoveringAllValues {
   public static Subarray
   findSmallestSequentiallyCoveringSubset(List<String> paragraph,
                                          List<String> keywords) {
-    // TODO - you fill in here.
-    return new Subarray(0, 0);
+    Map<String, Integer> keywordToIndex = new HashMap<>();
+    for (int i = 0; i < keywords.size(); i++) {
+      keywordToIndex.put(keywords.get(i), i);
+    }
+    Map<Integer, Integer> latestOccurrence = new HashMap<>();
+    Map<Integer, Integer> indexToDistance = new HashMap<>();
+    int index = 0;
+    Subarray subarray = new Subarray(-1, -1);
+    while (index < paragraph.size()) {
+      String word = paragraph.get(index);
+      if (keywordToIndex.containsKey(word)) {
+        int keywordIndex = keywordToIndex.get(word);
+        if (keywordIndex == 0) {
+          indexToDistance.put(0, 0);
+          latestOccurrence.put(keywordIndex, index);
+        } else if (indexToDistance.containsKey(keywordIndex - 1)) {
+          int distanceToPrevKeyword = index - latestOccurrence.get(keywordIndex - 1);
+          indexToDistance.put(keywordIndex, distanceToPrevKeyword + indexToDistance.get(keywordIndex - 1));
+          latestOccurrence.put(keywordIndex, index);
+        }
+        if (keywordIndex == keywords.size() - 1) {
+          if ((subarray.start == -1 && subarray.end == -1) || subarray.end - subarray.start > indexToDistance.get(keywordIndex)) {
+            if (indexToDistance.containsKey(keywordIndex)) {
+              subarray.start = index - indexToDistance.get(keywordIndex);
+              subarray.end = index;
+            }
+          }
+        }
+      }
+      index++;
+    }
+    return subarray;
   }
   @EpiTest(testDataFile = "smallest_subarray_covering_all_values.tsv")
   public static int findSmallestSequentiallyCoveringSubsetWrapper(

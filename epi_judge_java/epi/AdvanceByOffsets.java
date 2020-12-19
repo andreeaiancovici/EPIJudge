c625efbbf4af6c6e968d8a975b032bcd245ea916
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-12-20 00:03:21 +0200

    O(n*n) time complexity and O(n) space complexity

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -1,12 +1,24 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // TODO - you fill in here.
-    return true;
+    List<Integer> memo = new ArrayList<>(Collections.nCopies(maxAdvanceSteps.size(), 0));
+    memo.set(memo.size() - 1, 1);
+    for(int i = maxAdvanceSteps.size() - 2; i >= 0; i--) {
+      int jump = Math.min(maxAdvanceSteps.size() - 1, i + maxAdvanceSteps.get(i));
+      for(int j = i + 1; j <= jump; j++) {
+        if(memo.get(j) == 1) {
+          memo.set(i, 1);
+          break;
+        }
+      }
+    }
+    return memo.get(0) == 1;
   }
 
   public static void main(String[] args) {

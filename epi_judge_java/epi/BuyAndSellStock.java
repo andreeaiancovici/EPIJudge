31fa8f5b03d63fdafe58df20e0eb9be17f1ce61a
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-12-21 00:21:33 +0200

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -5,8 +5,12 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double minPrice = prices.get(0), maxProfit = 0;
+    for(double price : prices) {
+      minPrice = Math.min(minPrice, price);
+      maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
   }
 
   public static void main(String[] args) {

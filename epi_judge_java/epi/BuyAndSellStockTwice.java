0f7c6091fa38adf9e869ad5c82148913fac8721f
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2020-12-28 19:37:31 +0200

    O(n) time complexity and O(n*n) space complexity

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -5,8 +5,15 @@ import java.util.List;
 public class BuyAndSellStockTwice {
   @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
   public static double buyAndSellStockTwice(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double[][] profits = new double[3][prices.size()];
+    for (int j = 1; j <= 2; j++) {
+      double minPrice = prices.get(0);
+      for (int i = 1; i < prices.size(); i++) {
+        minPrice = Math.min(minPrice, prices.get(i) - profits[j - 1][i]);
+        profits[j][i] = Math.max(profits[j][i - 1], prices.get(i) - minPrice);
+      }
+    }
+    return profits[2][prices.size() - 1];
   }
 
   public static void main(String[] args) {

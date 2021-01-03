1b96fa13e0b5b6c15f90b2df484be27917af5ffa
Andreea Iancovici <andreea.steliana.iancovici@gmail.com>
2021-01-03 21:36:20 +0200

    O(n) time complexity and O(1) space complexity

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -8,27 +8,28 @@ import java.util.List;
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    if(perm.size() == 0) {
-      return new ArrayList<>();
+    if (perm.size() >= 2) {
+      if (perm.get(perm.size() - 1) > perm.get(perm.size() - 2)) {
+        Collections.swap(perm, perm.size() - 1, perm.size() - 2);
+      } else {
+        int i = perm.size() - 1;
+        while(i > 0 && perm.get(i) <= perm.get(i - 1)) {
+          i--;
+        }
+        if(i - 1 >= 0) {
+          int j = perm.size() - 1;
+          while (perm.get(j) <= perm.get(i - 1)) {
+            j--;
+          }
+          Collections.swap(perm, j, i - 1);
+          Collections.reverse(perm.subList(i, perm.size()));
+        } else {
+          return Collections.emptyList();
+        }
+      }
+    } else {
+      return Collections.emptyList();
     }
-
-    int i = perm.size() - 1;
-    while(i > 0 && perm.get(i) <= perm.get(i - 1)) {
-      i--;
-    }
-
-    if(i == 0) {
-      return new ArrayList<>();
-    }
-
-    int val = perm.get(i - 1);
-    int j = perm.size() - 1;
-    while(j > i - 1 && perm.get(j) <= val) {
-      j--;
-    }
-
-    Collections.swap(perm, i - 1, j);
-    Collections.reverse(perm.subList(i, perm.size()));
     return perm;
   }
 
